2aa5cdd69ba5bf90d21d9ed30da8f1b9496353a9
namg045 <namg045@newschool.edu>
2017-04-05 19:46:19 -0400

    i totally flunked this quiz

---
+++ projects/jsquiz/main.js
@@ -0,0 +1,18 @@
+<script type="text/javascript">
+
+var length = 3; //number
+var lastName = "Nam"; //string
+var x = {firstName: "Gyuri", lastName:"Nam"} //me
+true //boolean
+false //boolean
+
+
+
+function todaydate(){
+    var today_date= new Date()
+    var myyear=today_date.getYear()
+    var mymonth=today_date.getMonth()+1
+    var mytoday=today_date.getDate()
+    document.write(myyear+"/"+mymonth+"/"+mytoday)
+}
+</script>
